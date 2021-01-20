21f73810f41d6f6d07e2fb10cb1901287c4ab407
cindy jones <mzzjones@gmail.com>
2021-01-19 17:08:12 -0800

    my cursor ran away so I'd better commit before my work gets lost

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,23 +3,29 @@ import csv
 BudgetData_csv = os.path.join('Resources', 'budget_data.csv')
 
 # Read in the CSV file
-with open(BudgetData_csv, 'r') as csvfile:
+with open(BudgetData_csv, 'r', newline='') as csvfile:
 
-    # Split the data on commas
+    # Instantiate csv reader and split the data on commas
     csvreader = csv.reader(csvfile, delimiter=',')
 
+    # Remove header from the usable data
     header = next(csvreader)
    
     # Loop through the data
     for row in csvreader:
-# The total number of months included in the dataset
-        months_count = sum(1 for row in csvreader)
-        print(months_count)
 
-# The net total amount of "Profit/Losses" over the entire period
-    
+        # Assign variables to discrete data and assign profits/losses as float
+        months = row[0]
+        profits_losses = float(row[1]) 
+       
+        # Find total number of months 
+        
 
-# Calculate the changes in "Profit/Losses" over the entire period, then find the average of those changes
+        # The net total amount of "Profit/Losses" over the entire period
+        net_profits_losses += profits_losses
+        print(net_profits_losses)
+
+    # Calculate the changes in "Profit/Losses" over the entire period, then find the average of those changes
 
 
 # The greatest increase in profits (date and amount) over the entire period
